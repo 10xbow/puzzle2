d6bb4710d709509aeb458a94e0e1ef10d729e472
10xbow <10xbow@gmail.com>
2018-11-21 03:33:10 +0900

    q40途中

--- q40_oop/Program.cs
+++ q40_oop/Program.cs
@@ -12,6 +12,9 @@ namespace q40_oop
         {
             var Area = new Area(10, 10);
             var Tiles = new List<Tile> { new Tile(1, 1), new Tile(2, 2), new Tile(4, 2), new Tile(4, 4) };
+            Area.AddTile(Tiles[3]);
+            Area.Show();
+            Console.ReadLine();
         }
     }
 
@@ -28,8 +31,24 @@ namespace q40_oop
             Grid = Enumerable.Repeat(0, width).ToList();
         }
 
+        public void Show()
+        {
+            for (int i = 1; i <= Height; i++)
+            {
+                foreach (var item in Grid)
+                {
+                    Console.Write(item < i ? "□" : "■");
+                }
+                Console.WriteLine();
+            }
+        }
+
         public bool AddTile(Tile tile)
         {
+            for (int i = 0; i < tile.Width; i++)
+            {
+                Grid[i] += tile.Height;
+            }
             return true;
         }
     }
