5705db39eb462136eba986fe70e056b7457a5c48
10xbow <10xbow@gmail.com>
2018-11-21 00:02:34 +0900

    q40成功

--- q40/Program.cs
+++ q40/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace q40
 {
@@ -16,8 +14,36 @@ namespace q40
             int check(IList<int> island)
             {
                 var pos = (0, N);
-                var q = new List<(int, int)> { pos };
-
+                var q = new Queue<(int, int)>();
+                q.Enqueue(pos);
+                var log = new Dictionary<(int, int), int> { [pos] = 0 };
+                int left = 0;
+                int right = 0;
+                while (q.Count > 0) // 幅優先検索を実施
+                {
+                    (left, right) = q.Dequeue();
+                    for (int l = left - 1; l <= left + 1; l += 2)
+                    {
+                        for (int r = right - 1; r < right + 1; r += 2)
+                        {
+                            // 両方が同じ位置になれば終了
+                            if (l == r)
+                            {
+                                return log[(left, right)] + 2;
+                            }
+                            if ((l >= 0) && (r <= N) && (island[l] == island[r]))
+                            {
+                                if ((l < r) && !log.ContainsKey((l, r)))
+                                {
+                                    // AがBより左にあり、未探索であれば次を探索
+                                    q.Enqueue((l, r));
+                                    log[(l, r)] = log[(left, right)] + 2;
+                                }
+                            }
+                        }
+                    }
+                }
+                return -1; // 距離を求められなかった場合
             }
 
             int search(IList<int> island, int left, int level)
@@ -37,6 +63,9 @@ namespace q40
                 }
                 return max;
             }
+            var i = Enumerable.Repeat(0, N + 1).ToList();
+            Console.WriteLine(search(i, 0, 0));
+            Console.ReadLine();
         }
     }
 }
